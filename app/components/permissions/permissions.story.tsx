44ad5a8e10efe6675885f65150e5f948054160ca
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2020-04-08 20:46:04 +0200

    Added permission component

--- app/components/permissions/permissions.story.tsx
+++ app/components/permissions/permissions.story.tsx
@@ -3,14 +3,14 @@ import { storiesOf } from "@storybook/react-native"
 import { StoryScreen, Story, UseCase } from "../../../storybook/views"
 import { Permissions } from "./permissions"
 
-declare var module
+declare let module
 
 storiesOf("Permissions", module)
   .addDecorator(fn => <StoryScreen>{fn()}</StoryScreen>)
   .add("Style Presets", () => (
     <Story>
       <UseCase text="Primary" usage="The primary.">
-        <Permissions text="Permissions" />
+        <Permissions />
       </UseCase>
     </Story>
   ))
