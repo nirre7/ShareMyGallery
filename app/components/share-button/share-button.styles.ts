a15dc475f4f60e748c8a8908536dfd6866273bec
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2020-04-08 20:07:45 +0200

    Removed old version

--- app/components/share-button/share-button.styles.ts
+++
@@ -1,9 +0,0 @@
-import { ViewStyle } from "react-native"
-
-export const shareButtonStyles = {
-  WRAPPER: {
-    justifyContent: "center",
-    marginLeft: 20,
-    marginRight: 20,
-  } as ViewStyle,
-}
