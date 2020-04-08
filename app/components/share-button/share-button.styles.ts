2a0c10ec7c3af5fc13154c5987d77409f9d8c4b3
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2020-04-08 20:23:31 +0200

    Added share button

---
+++ app/components/share-button/share-button.styles.ts
@@ -0,0 +1,9 @@
+import { ViewStyle } from "react-native"
+
+export const shareButtonStyles = {
+  WRAPPER: {
+    justifyContent: "center",
+    marginLeft: 20,
+    marginRight: 20,
+  } as ViewStyle,
+}
